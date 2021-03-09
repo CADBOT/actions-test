266fc5cf34eaec9638c7cb0871f4b604c302a4a4
Charles Crawford <charles.crawford@tanium.com>
2021-03-08 19:36:03 -0800

    fix path

--- .github/actions/validate-pr-template/index.js
+++ .github/actions/validate-pr-template/index.js
@@ -1,18 +1,34 @@
 "use strict";
 
+/*
 const parsePullRequestId = githubRef => {
   const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
   if (!result) throw new Error("Reference not found.");
   const [, pullRequestId] = result;
   return pullRequestId;
 };
+*/
+const fs = require('fs')
+const ev = JSON.parse(
+  fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8')
+)
+const prNum = ev.pull_request.number
+console.log('PRNum', prNum);
 
-const main = async () => {
+function parsePullRequestId(githubRef) {
+  console.log(githubRef);
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
+  console.log(result);
+  if (!result) throw new Error("Reference not found.");
+  const [, pullRequestId] = result;
+  return pullRequestId;
+}
+
+const main = () => {
+  console.log(process.env.GITHUB_REF);
+  console.log('-----');
   const pullRequestId = parsePullRequestId(process.env.GITHUB_REF);
   console.log(pullRequestId);
 };
 
-main().catch(err => {
-  console.error(err);
-  process.exitCode = 1;
-});
+main();
