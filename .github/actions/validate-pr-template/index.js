17e2d4c2a6078f7ac6dda9a632b03ba493c45d46
Charles Crawford <charles.crawford@tanium.com>
2021-03-08 19:56:24 -0800

    more stuff

--- .github/actions/validate-pr-template/index.js
+++ .github/actions/validate-pr-template/index.js
@@ -21,17 +21,17 @@ console.log('PRNum', prNum);
 function parsePullRequestId(githubRef) {
   console.log(githubRef);
   const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
-  console.log('result', result);
+  console.log('result: ', result);
   if (!result) throw new Error("Reference not found.");
   const [, pullRequestId] = result;
   return pullRequestId;
 }
 
 const main = () => {
-  console.log(process.env.GITHUB_REF);
+  console.log('github ref: ', process.env.GITHUB_REF);
   console.log('-----');
   const pullRequestId = parsePullRequestId(process.env.GITHUB_REF);
-  console.log(pullRequestId);
+  console.log('prid: ', pullRequestId);
 };
 
 main();
